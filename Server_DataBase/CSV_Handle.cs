cfb67577b145d8bcce9da8d8c1d81122a82cf78a
Workrom <workrom24@gmail.com>
2024-04-30 00:05:24 +0300

    little by little

--- Server_DataBase/CSV_Handle.cs
+++ Server_DataBase/CSV_Handle.cs
@@ -1,43 +1,44 @@
-﻿
-using Microsoft.VisualBasic.FileIO;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 
 namespace Server_DataBase
 {
-    //UDTR - User, Date, Time, Reason.
     struct UDTR
     {
         public string User { get; set; }
         public string Date { get; set; }
         public string Time { get; set; }
         public string Reason { get; set; }
-
+        public int ReasonID { get; set; }
     }
+
     internal class CSV_Handle
     {
         public List<UDTR> DataList { get; } = new List<UDTR>();
         private string UDTpath = "C:\\Users\\Workrom\\source\\repos\\Server_DataBase\\Server_DataBase\\UDT.csv";
         private string RNpath = "C:\\Users\\Workrom\\source\\repos\\Server_DataBase\\Server_DataBase\\RN.csv";
 
-
-        public bool isLoaded(string user, string date, string time, string reason)
+        public bool isLoaded(string user, string date, string time, string reason, int reasonID)
         {
             if (string.IsNullOrWhiteSpace(user) || string.IsNullOrWhiteSpace(date) ||
-                string.IsNullOrWhiteSpace(time) || string.IsNullOrWhiteSpace(reason))
+                string.IsNullOrWhiteSpace(time) || string.IsNullOrWhiteSpace(reason) || reasonID <= 0)
             {
                 return false;
             }
+
             UDTR newData = new UDTR
             {
                 User = user,
                 Date = date,
                 Time = time,
-                Reason = reason
+                Reason = reason,
+                ReasonID = reasonID
             };
             DataList.Add(newData);
             return true;
-
         }
 
         public void Write(List<UDTR> dataList)
@@ -46,38 +47,49 @@ namespace Server_DataBase
             {
                 return;
             }
+            var existingReasons = new Dictionary<string, int>();
 
+            using (var reader = new StreamReader(RNpath))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    string[] parts = line.Split(',');
+                    existingReasons.Add(parts[1], int.Parse(parts[0]));
+                }
+            }
             using (StreamWriter stwUDT = new StreamWriter(UDTpath, true))
             using (StreamWriter stwRN = new StreamWriter(RNpath, true))
             {
                 for (int i = 0; i < dataList.Count; i++)
                 {
                     UDTR data = dataList[i];
-                    stwUDT.WriteLine($"{data.User},{data.Date},{data.Time}");
-                }
-                for (int i = 0; i < dataList.Count; i++)
-                {
-                    UDTR data = dataList[i];
-                    stwRN.WriteLine($"{data.Reason}");
+                    stwUDT.WriteLine($"{data.User},{data.Date},{data.Time},{data.ReasonID}");
+
+                    if (!existingReasons.ContainsKey(data.Reason))
+                    {
+                        int newReasonID = existingReasons.Count + 1;
+                        existingReasons.Add(data.Reason, newReasonID);
+                        stwRN.WriteLine($"{newReasonID},{data.Reason}");
+                    }
                 }
             }
         }
 
         public string Read()
         {
-
-            //Прошла неделя а непомню что тут за что отвечает
             StringBuilder bruh = new StringBuilder();
-            bruh.AppendLine("User, Date, Time, Reason");
-            var UDTstr = File.ReadLines(UDTpath).Skip(1).Select(LINE => LINE.Split(',')).Select(LINES => new UDTR {User = LINES[0], Date = LINES[1], Time = LINES[2] });
-            var RNstr = File.ReadLines(RNpath).Skip(1).Select(LINE => LINE.Split(',')).Select(LINES => new UDTR { Reason = LINES[0]});
-            var UDTRNstr = UDTstr.Zip(RNstr, (x, y) => new UDTR {User = x.User, Date = x.Date, Time = x.Time, Reason = y.Reason}).ToList();
-            foreach(var data in UDTRNstr)
+            bruh.AppendLine("User, Date, Time, Reason, ReasonID");
+
+            var UDTstr = File.ReadLines(UDTpath).Skip(1).Select(LINE => LINE.Split(',')).Select(LINES => new UDTR { User = LINES[0], Date = LINES[1], Time = LINES[2], ReasonID = int.Parse(LINES[3]) });
+            var reasons = File.ReadLines(RNpath).Skip(1).Select(line => line.Split(',')).ToDictionary(parts => int.Parse(parts[0]), parts => parts[1]);
+
+            foreach (var data in UDTstr)
             {
-                bruh.Append($"{data.User}, {data.Date}, {data.Time}, {data.Reason}\n");
+                string reason = reasons.ContainsKey(data.ReasonID) ? reasons[data.ReasonID] : "Unknown";
+                bruh.Append($"{data.User}, {data.Date}, {data.Time}, {reason}\n");
             }
             return bruh.ToString();
         }
-
     }
 }
